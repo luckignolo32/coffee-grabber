858eff7453664662f2713881ff3e7a0d266b2f29
luckignolo32 <luca.martinelli10@hotmail.com>
2025-10-28 15:44:43 +0200

    changed structure and added frontend/backend stuff

---
+++ backend/src/coffeeGrabber/main.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def main():
+    print("Hello from my_project!")
+    print(f"1 + 2 = {add(1, 2)}")
+
+if __name__ == "__main__":
+    main()
---
+++ backend/tests/test_main.py
@@ -0,0 +1,5 @@
+from coffeeGrabber.main import add
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
